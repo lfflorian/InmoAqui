92f2962db3c71ddb2d6031b0ee2b7ca7987a1e41
Luis Florian <lfflorian4@gmail.com>
2017-08-11 10:42:06 -0600

    utilizando json_encode

--- index.php
+++ index.php
@@ -4,16 +4,22 @@ include("connection.php");
 $sql = "SELECT operacion, tipoinmueble, titulo, descripcion, departamento, municipio, zona, colonia, nohabitaciones, nobanios, parqueos, dimenciones FROM inmueble";
 $result = $connection->query($sql);
 
+
+// Codigo utilizado para la carga de los inmuebles en un listado
+$inmuebles = array();
 if ($result->num_rows > 0)
 {
 	while ($row = $result->fetch_assoc())
 	{
-		echo $row["operacion"] ." ". $row["tipoinmueble"] ." ". $row["zona"];
+		// echo $row["operacion"] ." ". $row["tipoinmueble"] ." ". $row["zona"];
+		$inmuebles = $row;
 	}
 } else
 {
 	echo "ni un resultado";
 }
+print json_encode($inmuebles);
+// --- finaliza codigo 
 
-
+// Codigo 
  ?>
