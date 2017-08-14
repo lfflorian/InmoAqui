39ab1df1200c606c0babb770fb50fa522b2fff87
Luis Florian <lfflorian4@gmail.com>
2017-08-14 14:51:47 -0600

    agregando documento sql

--- prueba.php
+++ prueba.php
@@ -7,7 +7,7 @@
 <body>
     <section id="conent">dfa</section>
 
-
+    
 
     <script
   src="http://code.jquery.com/jquery-3.2.1.min.js"
@@ -16,18 +16,47 @@
 <script>
     $( document ).ready(function() {
         contenido();
-        
     });
 
+    var $Container = $('#conent');
     var contenido = function() {
         $.ajax({
             type:"POST",
             url:'datareturn.php',
             success:function(data){
-                console.log(data);
+                renderShows(data);
             }
         });
     }
+
+    var htmlContent = '<div>'+
+        '<h1>:titulo</h1>'+
+        '<h3>:operacion</h3><h3>:tipo</h3>'+
+        '<p>:descripcion</p>'+
+        '<strong>:cuartos</strong><strong>:banios</strong>'+
+        '</div>';
+
+    function renderShows(inmueble) {
+        // inmueble.forEach(function (inmueble) {
+
+            var datai = inmueble.data; 
+            for(var i in datai)
+            {
+                      var inmueble = htmlContent
+        .replace(':titulo:', i.titulo)
+        .replace(':operacion:', i.operacion)
+        .replace(':tipo:', i.tipo)
+        .replace(':descripcion:', i.descripcion)
+        .replace(':cuartos:', i.nohabitaciones)
+        .replace(':banios:', i.nobanios)
+      var $i = $(i)
+      $Container.append();
+
+            }
+
+    
+  }
+    
 </script>
 </body>
 </html>
